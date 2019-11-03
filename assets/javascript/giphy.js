27af42f912e74632fea0349d02f21da6b1b71d58
panda <ricky_lai@ymail.com>
2019-11-03 13:27:12 -0600

    final changes

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,104 +1,94 @@
 //store some buttons on the page to get the user started
-var btnArr = [
-    "The Office",
-    "The Ozark",
-    "John Wick",
-    "Breaking Bad",
-    "Fight Club"
+var searches = [
+    "Agriculture",
+    "Communism",
+    "Airports",
+    "Cactus",
+    "Banana",
+    "Royalty",
+    "Sports",
+    "Cobblers",
 ]
 
-//build a func to dynamically create a button for each 
-//string in the btnArr on load, and each time
-//the user adds a btn
+var userSearch = "";
 
-$(document).ready(function () {
+function buttons() {
+    $("#btnArea").empty();
 
-    function makeBtn() {
-
-        //loop to create a btn for each string in btnArr
-        for (i = 0; i < btnArr.length; i++) {
-            var addBtn = $("<button>");
-            addBtn.addClass("btn btn-primary movieBtn")
-            addBtn.text(btnArr[i]);
-            addBtn.attr("movieName", btnArr[i]);
-
-            //update the html with the new btn
-            $("#btnArea").append(addBtn);
-        }
+    for (var i = 0; i < searches.length; i++) {
+        $("#btnArea").append("<button type='button'>" + searches[i] + "</button>");
     }
-    //call the makeBtn func
-    makeBtn();
+}
+
+$("#subBtn").on("click", function () {
+    event.preventDefault();
+    searches.push($("#userIn").val().trim());
+    userSearch = $("#userIn").val().trim();
+    buttons();
+    api();
+    // console.log("dfjdlas");
+});
+
+$("body").on("click", "img", function () {
+
+    var current = $(this);
+    if (current.attr("data-state") === "still") {
+        current.attr("data-state", "play");
+        current.attr("src", current.attr("data-play"));
+    }
+
+    else if (current.attr("data-state") === "play") {
+        current.attr("data-state", "still");
+        current.attr("src", current.attr("data-still"));
 
 
+    }
+});
 
-    //need to be able to add a button---
-    //take the text the user inputs and add it to btnArr
-    //when the submit btn is clicked
-    $("#subBtn").on("click", function (event) {
-        event.preventDefault();
-        btnArr.push($("#inputMovie").val().trim())
-        $("#btnArea").empty();
-        makeBtn();
-        // reset the inputMovie area to blank
-        $("#inputMovie").val("")
+$("#btnArea").on("click", "button", function () {
+
+    var current2 = $(this).text();
+    userSearch = current2;
+    api();
+
+
+});
+
+function api() {
+    console.log(userSearch);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=s8PI78OwluPEkf244n0XuATmutASEWg6&q=" + userSearch + "&limit=10&offset=0&rating=G&lang=en"
+
+    $.ajax({
+        url: queryURL,
+        type: "GET"
+    }).then(function (response) {
+        var data = response.data;
+        for (var i = 0; i < 10; i++) {
+            var rating = data[i].rating;
+            var title = data[i].title;
+            var image = data[i].images.fixed_height.url;
+            var still = data[i].images.fixed_height_still.url;
+
+            var final = $("<div>");
+            var img = $("<img src='" + still + "'data-play='" + image + "'data-still='" + still + "'data-state='still'>");
+            var text = $("<p>" + title + "</p>");
+            var text2 = $("<p>" + rating + "</p>");
+
+
+            final.append(img, text, text2);
+            $("#gifArea").append(final);
+        }
+
+
+        console.log(response);
+
     });
 
 
+}
+buttons();
 
-    //for when one of the movie buttons is clicked:
-    //on click of button, AJAX req for search of giphy API for the text on that button
-    $(document).on("click", ".movieBtn", function () {
-        //console.log("hi");
-        $("#gifArea").empty();
-        var movieName = $(this).attr("movieName");
-        var apiKey = "l4rTiwy6XT6KwlkOqVtLThMThBJYqrhU";
-        // var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + movieName + "&limit=10&rating=g" ;
 
-        var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${movieName}&limit=10`;
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response.data);
-            var data = response.data;
-            var rating = data.rating;
-            for (i = 0; i < data.length; i++) {
 
-                var newImg = $("<img height= '250px' width= '250px'>");     //dynamically creates img with width and height req
-                var p = $("<p>").text("Rating: " + data[i].rating.toUpperCase());         //get the rating from the response
 
-                //adding classes to the new GIFs. adding a src attr so it pulls the STILL version.
-                newImg.addClass("img-fluid movieGif").attr("src", data[i].images.fixed_width_still.url)
-                    .attr("alt", movieName)
-                    .attr("state", "still")
-                    .attr("still", data[i].images.fixed_width_still.url)
-                    .attr("animated", data[i].images.fixed_width.url);
-                p.addClass("ratings");
-                $("#gifArea").append(newImg, p);
-
-            }
-        });
-
-    })
-    //click handler for when a GIF is clicked. 
-    $(document).on("click", ".movieGif", function () {
-
-        var state = $(this).attr("state");          //creating state var for a boolean
-
-        // if the state is still, which we know it is when it is created, then 
-        // replace the still src with animated when clicked AND
-        // give the state attr the value of animated from above (the animated version of the gif)
-
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("animated"))
-            $(this).attr("state", "animated")
-        }
-        // if the state does not = still when clicked, give the img the still attr
-        // and set the state attr to a value of still
-        else {
-            $(this).attr("src", $(this).attr("still"))
-            $(this).attr("state", "still")
-        }
-    })
-});
